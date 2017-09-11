0b325b16e00f99d3b06240253847ecfc15ad6329
Tad Scritchfield <tadscritch@gmail.com>
2017-09-10 18:33:51 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import ContactList from './ContactList';
+import logo from './sibi.png';
+import './App.css';
+
+const { Header, Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Header style={{ paddingTop: "14px", paddingBottom: "14px" }}>
+            <img src={logo} style={{ height: "100%" }} alt="logo"></img>
+          </Header>
+          <Content>
+            <ContactList></ContactList>
+          </Content>
+        </Layout>
+      </div>
+
+    );
+  }
+}
+
+export default App;
